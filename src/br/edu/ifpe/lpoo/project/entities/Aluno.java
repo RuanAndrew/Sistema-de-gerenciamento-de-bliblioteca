efff776ee4f69656acaa102ee3f5e4b6829f770e
Eduardo Henrique <eduardohgc3@gmail.com>
2025-06-13 20:54:16 -0300

    Inserido classe Aluno

--- src/br/edu/ifpe/lpoo/project/entities/Aluno.java
+++ src/br/edu/ifpe/lpoo/project/entities/Aluno.java
@@ -1,5 +1,66 @@
 package br.edu.ifpe.lpoo.project.entities;
 
-public class Aluno {
+import java.time.LocalDate;
 
+public class Aluno {
+	
+	private String cpf;
+	private String nome;
+	private String telefone;
+	private String email;
+	private LocalDate data;
+	
+	public Aluno(String cpf, String nome, String telefone, String email, LocalDate data) {
+		super();
+		this.cpf = cpf;
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+		this.data = data;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+	
 }
+
+
