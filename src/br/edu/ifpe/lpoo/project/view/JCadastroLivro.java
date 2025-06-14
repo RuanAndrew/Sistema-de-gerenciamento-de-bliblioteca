e002c502423576bbe1237f0e6fadcfe6bd8957df
Eduardo Henrique <eduardohgc3@gmail.com>
2025-06-13 21:01:20 -0300

    Iniciando tela de Cadastro de livros

---
+++ src/br/edu/ifpe/lpoo/project/view/JCadastroLivro.java
@@ -0,0 +1,55 @@
+package br.edu.ifpe.lpoo.project.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class JCadastroLivro {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JCadastroLivro window = new JCadastroLivro();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public JCadastroLivro() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel isbnLabel = new JLabel("ISBN");
+		isbnLabel.setBounds(105, 128, 45, 13);
+		frame.getContentPane().add(isbnLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(105, 140, 96, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module SistemaGenrenciamentoBiblioteca {
+	requires java.desktop;
 }
