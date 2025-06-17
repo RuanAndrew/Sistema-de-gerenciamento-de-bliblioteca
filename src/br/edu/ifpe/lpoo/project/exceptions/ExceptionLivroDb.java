64ff08be0ebf83b2720b843a2be9d395ebf38687
Eduardo Henrique <eduardohgc3@gmail.com>
2025-06-17 10:26:23 -0300

    Exceção personalizada conexão banco

---
+++ src/br/edu/ifpe/lpoo/project/exceptions/ExceptionLivroDb.java
@@ -0,0 +1,10 @@
+package br.edu.ifpe.lpoo.project.exceptions;
+
+public class ExceptionLivroDb extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionLivroDb(String message) {
+		super(message);
+	}
+}
