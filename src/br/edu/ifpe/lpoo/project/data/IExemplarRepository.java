e6873464ed15f5b38cfa7ed63e8497b908c618ad
Eduardo Henrique <eduardohgc3@gmail.com>
2025-07-02 09:54:34 -0300

    Métodos de busca

--- src/br/edu/ifpe/lpoo/project/data/IExemplarRepository.java
+++ src/br/edu/ifpe/lpoo/project/data/IExemplarRepository.java
@@ -1,14 +1,21 @@
 package br.edu.ifpe.lpoo.project.data;
 
+import java.util.List;
+
 import br.edu.ifpe.lpoo.project.entities.acervo.Exemplar;
 
 public interface IExemplarRepository {
 
-	boolean existItem(String indentifier);
-	
 	void insert(Exemplar exemplar, int idLivro);
-	
-	void deleteParaLivros(int idLivro);
-	
+
+	boolean existItem(Exemplar exemplar);
+
 	void atualizarStatus(Exemplar exemplar);
+
+	Exemplar buscarPorId(int idExemplar);
+
+	void deleteParaLivros(int idLivro);
+
+	List<Exemplar> buscarTodosPorIdLivro(int idItem);
+
 }
