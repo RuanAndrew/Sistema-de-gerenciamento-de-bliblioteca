21f85c02297c82a13fab724564bc227e51e220cc
Eduardo Henrique <eduardohgc3@gmail.com>
2025-07-03 14:20:43 -0300

    Teste

---
+++ src/br/edu/ifpe/lpoo/project/data/implement/IAlunoRepository.java
@@ -0,0 +1,22 @@
+package br.edu.ifpe.lpoo.project.data.implement;
+
+import java.util.List;
+
+import br.edu.ifpe.lpoo.project.entities.membros.Aluno;
+
+public interface IAlunoRepository {
+	
+	void insert (Aluno aluno);
+	
+	boolean existMembro(Aluno aluno);
+	
+	void delete(int idMembro);
+	
+	Aluno buscarPorId(int idMembro);
+	
+	List<Aluno> buscarTodos();
+	
+	List<Aluno> buscarPorTermo(String termo);
+	
+	void atualizar(Aluno aluno);
+}
