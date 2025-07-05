68728f8efcbfe2011a04d099b309b5a0452b0ce0
Eduardo Henrique <eduardohgc3@gmail.com>
2025-07-05 16:54:40 -0300

    Ajustando métodos de EmprestimoRepository

--- src/br/edu/ifpe/lpoo/project/data/acervo/implement/EmprestimoRepository.java
+++ src/br/edu/ifpe/lpoo/project/data/acervo/implement/EmprestimoRepository.java
@@ -11,8 +11,7 @@ import java.time.LocalDate;
 import br.edu.ifpe.lpoo.project.data.ConnectionDb;
 import br.edu.ifpe.lpoo.project.data.IEmprestimoRepository;
 import br.edu.ifpe.lpoo.project.entities.gerenciamento.Emprestimo;
-import br.edu.ifpe.lpoo.project.enums.TipoItemAcervo;
-import br.edu.ifpe.lpoo.project.enums.TipoMembro;
+import br.edu.ifpe.lpoo.project.enums.StatusEmprestimo;
 import br.edu.ifpe.lpoo.project.exceptions.DbException;
 
 public class EmprestimoRepository implements IEmprestimoRepository {
@@ -24,41 +23,33 @@ public class EmprestimoRepository implements IEmprestimoRepository {
 			throw new DbException("Objeto tipo Emprestimo não pode ser null");
 		}
 
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rst = null;
-
 		int idEmprestimo = -1;
 
-		String consulta = "INSERT INTO emprestimo (id_item, id_membro, tipo_item, tipo_membro, data_emprestimo, data_para_devolucao, data_devolucao) "
-				+ "VALUES (?, ?, ?, ?, ?, ?, ?)";
+		String sqlEmprestimo = "INSERT INTO emprestimo (id_item, id_membro, data_emprestimo, data_para_devolucao, data_devolucao, status_emprestimo) "
+				+ "VALUES (?, ?, ?, ?, ?, ?)";
 
-		try {
-			conn = ConnectionDb.getConnection();
-			stmt = conn.prepareStatement(consulta, Statement.RETURN_GENERATED_KEYS);
+		try (Connection conn = ConnectionDb.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sqlEmprestimo, Statement.RETURN_GENERATED_KEYS)) {
 
 			stmt.setInt(1, emprestimo.getIdItemAcervo());
 			stmt.setInt(2, emprestimo.getIdMembro());
-			stmt.setString(3, emprestimo.getTipoItemAcervo().name());
-			stmt.setString(4, emprestimo.getTipoMembro().name());
-			stmt.setDate(5, Date.valueOf(emprestimo.getDataEmprestimo()));
-			stmt.setDate(6, Date.valueOf(emprestimo.getDataParaDevolucao()));
-			stmt.setDate(7, null);
-
+			stmt.setDate(3, Date.valueOf(emprestimo.getDataEmprestimo()));
+			stmt.setDate(4, Date.valueOf(emprestimo.getDataParaDevolucao()));
+			stmt.setDate(5, null);
+			stmt.setString(6, emprestimo.getStatusEmprestimo().name());
 			stmt.executeUpdate();
 
-			rst = stmt.getGeneratedKeys();
+			try (ResultSet rst = stmt.getGeneratedKeys()) {
 
-			while (rst.next()) {
-				idEmprestimo = rst.getInt(1);
+				if (rst.next()) {
+					idEmprestimo = rst.getInt(1);
+				} else {
+					throw new DbException("Erro ao gerar id");
+				}
 			}
 
 		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
-		} finally {
-			ConnectionDb.closeResultSet(rst);
-			ConnectionDb.closeStatement(stmt);
-			ConnectionDb.closeConnection(conn);
+			throw new DbException("Erro ao insrir um novo emprestimo. Causado por: " + e.getMessage());
 		}
 		return idEmprestimo;
 	}
@@ -70,34 +61,27 @@ public class EmprestimoRepository implements IEmprestimoRepository {
 			throw new DbException("Objeto tipo Emprestimo não pode ser null");
 		}
 
-		Connection conn = null;
-		PreparedStatement stmt = null;
-
-		String consulta = "UPDATE emprestimo "
-				+ "SET id_item = ?, id_membro = ? , tipo_item = ?, tipo_membro = ?, data_emprestimo = ?, data_para_devolucao = ?, data_devolucao = ? "
+		String sqlEmprestimo = "UPDATE emprestimo "
+				+ "SET id_item = ?, id_membro = ? , data_emprestimo = ?, data_para_devolucao = ?, data_devolucao = ?, status_emprestimo = ? "
 				+ "WHERE id_emprestimo = ?";
 
-		try {
-
-			conn = ConnectionDb.getConnection();
-			stmt = conn.prepareStatement(consulta);
+		try (Connection conn = ConnectionDb.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sqlEmprestimo)) {
 
 			stmt.setInt(1, emprestimo.getIdItemAcervo());
 			stmt.setInt(2, emprestimo.getIdMembro());
-			stmt.setString(3, emprestimo.getTipoItemAcervo().name());
-			stmt.setString(4, emprestimo.getTipoMembro().name());
-			stmt.setDate(5, emprestimo.getDataEmprestimo() != null ? Date.valueOf(emprestimo.getDataEmprestimo()) : null);
-			stmt.setDate(6, emprestimo.getDataParaDevolucao() != null ? Date.valueOf(emprestimo.getDataParaDevolucao()) : null);
-			stmt.setDate(7, emprestimo.getDataDevolucao() != null ? Date.valueOf(emprestimo.getDataDevolucao()) : null);
-			stmt.setInt(8, emprestimo.getIdEmprestimo());
+			stmt.setDate(3,
+					emprestimo.getDataEmprestimo() != null ? Date.valueOf(emprestimo.getDataEmprestimo()) : null);
+			stmt.setDate(4,
+					emprestimo.getDataParaDevolucao() != null ? Date.valueOf(emprestimo.getDataParaDevolucao()) : null);
+			stmt.setDate(5, emprestimo.getDataDevolucao() != null ? Date.valueOf(emprestimo.getDataDevolucao()) : null);
+			stmt.setString(6, emprestimo.getStatusEmprestimo().name());
+			stmt.setInt(7, emprestimo.getIdEmprestimo());
 
 			stmt.executeUpdate();
 
 		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
-		} finally {
-			ConnectionDb.closeStatement(stmt);
-			ConnectionDb.closeConnection(conn);
+			throw new DbException("Erro ao atualizar emprestimo. Causado por: " + e.getMessage());
 		}
 	}
 
@@ -107,52 +91,40 @@ public class EmprestimoRepository implements IEmprestimoRepository {
 		if (idEmprestimo <= 0) {
 			throw new DbException("O id inválido");
 		}
-
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rst = null;
-
+		
 		Emprestimo emprestimo = null;
 
-		String consulta = "SELECT * FROM emprestimo WHERE id_emprestimo = ?";
+		String sqlEmprestimo = "SELECT * FROM emprestimo WHERE id_emprestimo = ?";
 
-		try {
-			conn = ConnectionDb.getConnection();
-			stmt = conn.prepareStatement(consulta);
+		try (Connection conn = ConnectionDb.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sqlEmprestimo)) {
 
 			stmt.setInt(1, idEmprestimo);
 
-			rst = stmt.executeQuery();
+			try (ResultSet rst = stmt.executeQuery()) {
 
-			if (rst.next()) {
+				if (rst.next()) {
 
-				int id = rst.getInt("id_emprestimo");
-				int idItemAcervo = rst.getInt("id_item");
-				int idMembro = rst.getInt("id_membro");
-				String itemAcervo = rst.getString("tipo_item").toUpperCase();
-				TipoItemAcervo tipoItemAcervo = TipoItemAcervo.valueOf(itemAcervo);
-				String membro = rst.getString("tipo_membro");
-				TipoMembro tipoMembro = TipoMembro.valueOf(membro);
-				LocalDate dataEmprestimo = rst.getDate("data_emprestimo").toLocalDate();
-				LocalDate dataParaDevolucao = rst.getDate("data_para_devolucao").toLocalDate();
-				Date dataDevolucaoSql = rst.getDate("data_devolucao");
-				LocalDate dataDevolucao = dataDevolucaoSql != null ? dataDevolucaoSql.toLocalDate() : null;
+					int id = rst.getInt("id_emprestimo");
+					int idItemAcervo = rst.getInt("id_item");
+					int idMembro = rst.getInt("id_membro");
+					LocalDate dataEmprestimo = rst.getDate("data_emprestimo").toLocalDate();
+					LocalDate dataParaDevolucao = rst.getDate("data_para_devolucao").toLocalDate();
+					Date dataDevolucaoSql = rst.getDate("data_devolucao");
+					LocalDate dataDevolucao = dataDevolucaoSql != null ? dataDevolucaoSql.toLocalDate() : null;
+					String status = rst.getString("status_emprestimo").toUpperCase();
+					StatusEmprestimo statusEmprestimo = StatusEmprestimo.valueOf(status);
 
-				emprestimo = new Emprestimo(idItemAcervo, idMembro, tipoItemAcervo, tipoMembro, dataEmprestimo,
-						dataParaDevolucao, dataDevolucao);
-				emprestimo.setIdEmprestimo(id);
-
+					emprestimo = new Emprestimo(idItemAcervo, idMembro, dataEmprestimo, dataParaDevolucao,
+							dataDevolucao, statusEmprestimo);
+					emprestimo.setIdEmprestimo(id);
+				}
 			}
 
 		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
-		} finally {
-			ConnectionDb.closeResultSet(rst);
-			ConnectionDb.closeStatement(stmt);
-			ConnectionDb.closeConnection(conn);
+			throw new DbException("Erro ao buscar empréstimo por id. Causado por: " + e.getMessage());
 		}
 
 		return emprestimo;
 	}
-
 }
